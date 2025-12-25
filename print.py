083c299033420e876bc770ba16d2aa195a693401
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-12-25 20:52:11 +0600

    code

---
+++ print.py
@@ -0,0 +1,10 @@
+age = 45
+interest_rate = 2.5
+name = 'Mahia Momo'
+district = "Dhaka"
+is_single = True
+is_sleeping = False 
+
+print('Kodom Ali' + ' ' + 'kacha badam')
+text = f"kodom ali {age} living in {district}.interesting with rate{interest_rate}%"
+print(text)
