1cf58202f5c73d4464c382fe71d44d60471456ee
mahiamOmO <129661915+mahiamOmO@users.noreply.github.com>
2025-12-25 20:42:18 +0600

    variable.py

---
+++ variable.py
@@ -0,0 +1,14 @@
+age = 45
+interest_rate = 2.5
+name = 'Mahia Momo'
+district = "Dhaka"
+is_single = True
+is_sleeping = False 
+
+print(age)
+print(age + interest_rate)
+print(interest_rate)
+print(name)
+print(district)
+print(is_single)
+print(is_sleeping)
